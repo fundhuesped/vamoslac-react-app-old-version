861eea9ca5f5f0358fcd9e3eb6a21e24ce678d97
Enzzo Perez <enzzorp@gmail.com>
2019-07-30 22:01:17 -0300

    added images

---
+++ android/app/src/main/java/com/vamos/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.vamoslac.myapp;
+
+/**
+ * Created by fede on 9/9/17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
